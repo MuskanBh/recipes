a33c686d7b481155ec77185016dcdf80ac4d7baa
MuskanBh <muskanbharalawala10@gmail.com>
2022-12-27 16:59:56 +0530

    Http Request to fetch data from Db

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, tap } from 'rxjs/operators';
+import { Recipe } from '../recipes/recipe.model';
 import { RecipeService } from '../recipes/recipe.service';
+
 
 @Injectable({
   providedIn:'root'
@@ -24,8 +27,18 @@ export class DataStorageService{
     });
   }
   fetchRecipes(){
-    return this.http.get('https://recipe-book-9f92f-default-rtdb.firebaseio.com/recipes.json').subscribe(recipes => {
-      console.log(recipes);
-    });
+     return this.http.get<Recipe[]>('https://recipe-book-9f92f-default-rtdb.firebaseio.com/recipes.json').pipe(
+      map(recipes => {
+        return recipes.map(recipe => {
+          return {
+            ...recipe,
+            ingredients: recipe.ingredients ? recipe.ingredients : []
+          };
+        });
+      }),
+      tap(recipes => {
+        this.recipeService.setRecipes(recipes);
+      })
+    )
   }
 }
